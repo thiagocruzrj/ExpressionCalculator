ba748aa1e6d0d680a4786d8412763acfffa86cc9
Thiago Cruz <thagocruz@gmail.com>
2021-09-12 14:49:45 -0300

    feat: add calculator run process

--- PeerIslands.ExpressionCalculator/Program.cs
+++ PeerIslands.ExpressionCalculator/Program.cs
@@ -6,7 +6,37 @@ namespace PeerIslands.ExpressionCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Run();
+        }
+
+        static void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("Write an expression to calculate or type 'quit' to exit:");
+                var calculator = new Calculator();
+                var stringExpression = Console.ReadLine();
+
+                switch (stringExpression)
+                {
+                    case "quit":
+                        return;
+
+                    default:
+                        try
+                        {
+                            var result = calculator.Calculate(stringExpression);
+                            Console.WriteLine($"{stringExpression} = {result}");
+
+                            continue;
+                        }
+                        catch (FormatException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            continue;
+                        }
+                }
+            }
         }
     }
 }
